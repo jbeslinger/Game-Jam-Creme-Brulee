946981b77854ee7669c7159f7d185b0c28645ee2
jbeslinger <jbeslinger@gmail.com>
2021-11-29 00:45:40 -0500

    Game now neglects the direction to push pieces because that mechanic is moot.

--- Assets/Scripts/Behaviors/PlayerInputBehavior.cs
+++ Assets/Scripts/Behaviors/PlayerInputBehavior.cs
@@ -66,9 +66,7 @@ public class PlayerInputBehavior : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
             if (_selectedObj != null && hit.collider != null && hit.collider.gameObject.GetComponent<PieceBehavior>() != null)
             {
-                if (_hasRequestedPreview &&
-                    (hit.collider.gameObject != _objUnderCursor ||
-                    _directionToPush != CalculatePushDirection(hit.collider.gameObject, mousePos)))
+                if (_hasRequestedPreview && (hit.collider.gameObject != _objUnderCursor))
                 {
                     EndPreview();
                 }
@@ -77,7 +75,6 @@ public class PlayerInputBehavior : MonoBehaviour
                     if (!hit.collider.GetComponent<PieceBehavior>().Hardened)
                     {
                         _objUnderCursor = hit.collider.gameObject;
-                        _directionToPush = CalculatePushDirection(_objUnderCursor, mousePos);
                         PreviewPlacement();
                     }
                 }
@@ -147,49 +144,6 @@ public class PlayerInputBehavior : MonoBehaviour
             _hasRequestedPreview = false;
             _gb.RemovePreviewPlacement();
         }
-    }
-
-    /// <summary>
-    /// Returns the direction which the object would be pushed given the current mouse position; based on angle of approach.
-    /// </summary>
-    /// <param name="objectToPush"></param>
-    /// <param name="currentMousePos"></param>
-    /// <returns></returns>
-    private PushDirection CalculatePushDirection(GameObject objectToPush, Vector2 currentMousePos)
-    {
-        float angle = CalculateAngleBetweenPoints(objectToPush.transform.position, currentMousePos);
-        if (angle <= 135f && angle > 45f)
-        {
-            return PushDirection.DOWN;
-        }
-        else if ((angle <= 45f && angle >= 0f) || (angle <= 360f && angle > 315f))
-        {
-            return PushDirection.LEFT;
-        }
-        else if (angle <= 315f && angle > 225f)
-        {
-            return PushDirection.UP;
-        }
-        else
-        {
-            return PushDirection.RIGHT;
-        }
-    }
-
-    /// <summary>
-    /// Return an angle between two positions: 0 to 360.
-    /// </summary>
-    /// <param name="posA"></param>
-    /// <param name="posB"></param>
-    /// <returns></returns>
-    private float CalculateAngleBetweenPoints(Vector2 posA, Vector2 posB)
-    {
-        float angle = Mathf.Atan2(posB.y - posA.y, posB.x - posA.x) * 180 / Mathf.PI;
-        if (angle < 0)
-        {
-            angle += 360;
-        }
-        return angle;
     }
     #endregion
 }
